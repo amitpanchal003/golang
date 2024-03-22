eda0bc840a675a4ce5d7cae66987a8b7e89b82b9
amit <pmitenrique@gmail.com>
2024-03-22 13:15:48 +0000

    learn about variables

--- Golang/01_Day/day_o1.go
+++ Golang/01_Day/day_o1.go
@@ -1,1 +1,40 @@
-hello this is day one of goLang Series.
+// hello this is day one of goLang Series.
+package main
+
+// it is a special package that is used to create executable programs.
+// it is entry point for any executable program.
+// it must contain functiom called main().
+import "fmt"
+
+// it provide input and output function.
+
+func main() { // This is main method{function} this is main entry point of program.
+	var age int = 28 //variable declaration and assign the value
+	/*var is a keyword
+	  age is varable name.
+	  int is data type.
+	  = is operator
+	  28 is value.
+
+	*/
+	var name string = "Hi amit"
+	// string type variable declaration and assignment
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println("hello world") // This is output statement
+}
+
+/*
+#some important things to keep in mind while declaring variables.
+01. variable must br declared before they can be used.
+02. the type of variable must be specified.
+03. variable can de declared outside(global variable) as well as inside(local variable) the function
+04. global batiabel can be accessed any function in the same package.
+05. local variable only acced by the function in which they declared.
+06. variable can be initialised with a value when they are declared,
+		or they can be assigned value later.
+07. if the variable is not initialised, it'll be assigned zero(0) value for it.
+08. the zero value for int is 0(int=0)
+	for string ("").
+	for boolean (false)
+*/
