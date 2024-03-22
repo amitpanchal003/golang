6012b686bcf65fb9be2e40d5fba90f5e7bf0ce51
amit <pmitenrique@gmail.com>
2024-03-22 14:37:27 +0000

    composit data type

--- Golang/02_Day/day_02.go
+++ Golang/02_Day/day_02.go
@@ -19,7 +19,7 @@
 
 */
 //Problem #1: We want to populate the information of a student.
-package main
+/*package main
 
 import "fmt"
 
@@ -36,4 +36,74 @@ func main() {
 	gpa = 3.9
 	isAdult = true
 	fmt.Println(name, age, address, gpa, isAdult)
+}
+*/
+
+/*
+#####################################################################
+#####################################################################
+Array:
+An array is a systematic arrangement of similar objects,
+usually in rows and columns.
+>> the type[n]T is an array of n value of type T
+
+	i.e. var a [5]int
+	here,
+	a = is variable name.
+	[5]= is size of array.
+	int = is data type
+
+package main
+
+import "fmt"
+
+func main() {
+	var list1 [2]string //----array declaration
+	list1[0] = "hello"  // ----array initialization
+	list1[1] = "amit"   // -----array initialization
+
+	fmt.Println(list1) // -------printing the value of array
+
+	//---------------------another way
+
+	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+
+	//---------------------using shorthand
+
+	primeNumbers := [5]int{2, 3, 5, 7, 11}
+	fmt.Println(primeNumbers)
+}
+*/
+/* ###############################################
+   ###############################################
+
+>> Slices:
+	_In Go language slice is more powerful, flexible,
+	convenient than an array, and is a lightweight data structure.
+	 Slice is a variable-length sequence that stores elements of a
+	 similar type, you are not allowed to store different type of
+	 elements in the same slice.
+
+
+*/
+package main
+
+import "fmt"
+
+func main() { // Array
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+	//struct
+	type person struct {
+		name string
+		age  int
+	}
+	var p1 person = person{name: "Bob", age: 28}
+	fmt.Println(p1)
+	// Map
+	var data1 map[string]int = map[string]int{"age": 28, "size": 180}
+	fmt.Println(data1)
 }
