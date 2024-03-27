b0b9c64dc489694b7903692b08e9e80f3df6834a
amit <pmitenrique@gmail.com>
2024-03-27 09:38:05 +0000

    time date methods

--- Golang/05_Day/main.go
+++ Golang/05_Day/main.go
@@ -13,4 +13,10 @@ func main() {
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
 	// to gate date time and day in human readable format
 	// we have to pass this parameter in required format
+
+	// creating time and date
+	createDate := time.Date(2020, time.November, 22, 06, 12, 0, 0, time.UTC)
+
+	fmt.Println(createDate)
+	fmt.Println(createDate.Format("01-02-2006 Monday"))
 }
