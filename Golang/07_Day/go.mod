28129be233315cc0169ebae4bc356005e07f565f
amit <pmitenrique@gmail.com>
2024-03-29 10:03:53 +0000

    simple if statement

---
+++ Golang/07_Day/go.mod
@@ -0,0 +1,3 @@
+module conditional
+
+go 1.22.1
---
+++ Golang/07_Day/main.go
@@ -0,0 +1,25 @@
+// conditional statement in golang.
+// 01.if statement.
+// syntax
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("learnig conditional statement in golang")
+
+	// amitHasMoney := true
+
+	// if amitHasMoney == true {
+	// 	fmt.Println("eligible to live in society")
+
+	// }
+	grade := 70 //initialization of int type variable
+
+	if grade >= 65 { // if this condition become true then only it execute inner block
+		fmt.Println("passing marks") //print the statement
+
+	}
+}
