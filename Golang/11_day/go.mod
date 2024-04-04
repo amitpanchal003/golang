84679c47a0f7239f40d10f89cfa76ba36234b578
amit <pmitenrique@gmail.com>
2024-04-04 08:16:24 +0100

    started dummy project

---
+++ Golang/11_day/go.mod
@@ -0,0 +1,5 @@
+module github.com/amitpanchal003/buildapi
+
+go 1.22.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ Golang/11_day/main.go
@@ -0,0 +1,23 @@
+package main
+
+// model for course-- usually this goes in seprate file
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+//fake database
+
+var courses []Course
+
+func main() {
+
+}
