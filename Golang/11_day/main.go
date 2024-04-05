e1724f9748e19512bac83cebdf14f536e7acf8a4
amit <pmitenrique@gmail.com>
2024-04-05 12:09:44 +0100

    configure routes

--- Golang/11_day/main.go
+++ Golang/11_day/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // model for course-- usually this goes in seprate file
 
 type Course struct {
@@ -20,10 +26,22 @@ type Author struct {
 
 var courses []Course
 
-//middleware or helper file -- this goes in seprate file
+// middleware or helper file -- this goes in seprate file
 func (c *Course) IsEmpty() bool {
-
+	return c.CourseId == "" && c.CourseName == ""
 }
 func main() {
 
+}
+
+// controllers
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>welcome to building api page</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
